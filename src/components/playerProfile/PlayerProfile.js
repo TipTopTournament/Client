8b4b0d4a4d5deff128b5dc4844822b849c1d9b9a
Tony <tonyylyy@gmail.com>
2020-04-19 19:50:24 +0200

    imported modules in playerProfile

--- src/components/playerProfile/PlayerProfile.js
+++ src/components/playerProfile/PlayerProfile.js
@@ -1,13 +1,12 @@
+import withRouter from "react-router-dom";
+import React from "react";
 
 class PlayerProfile extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            player: null,
-        };
-    }
-
-
+  constructor() {
+    super();
+    this.state = {
+      player: null
+    };
+  }
 }
 export default withRouter(PlayerProfile);
