e870886b88500ee562883e68387aa8e34154a7bd
S Anzolut <stefano.anzolut@gmail.com>
2020-04-21 11:23:07 +0200

    Home Screen now responsive

--- src/components/homescreen/HomeScreen.js
+++ src/components/homescreen/HomeScreen.js
@@ -1,55 +1,22 @@
 import styled from "styled-components";
 import React from "react";
 import {withRouter} from "react-router-dom";
-import {ButtonWhite} from "../../views/design/ButtonWhite";
-
-
+import {Button} from "../../views/design/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 /** These four components are HomeScreen specific not necessary to make View class for it*/
-
-const BaseContainer = styled.div`
-  margin-top: 2em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 300px;
-  justify-content: center;
-  max-width: auto;
-  max-height: auto;
-  flex-direction: column;
-  color: #2F80ED;
-`;
-
-const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 1440px;
-  height: 800px;
-  font-size: 16px;
-  font-weight: 300;
-  padding-left: 37px;
-  padding-right: 37px;
-  border-radius: 5px;
-  background: #2F80ED;
-  transition: opacity 0.5s ease, transform 0.5s ease;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-`;
 
 const Title = styled.div`
 font-family: Roboto;
 font-style: normal;
 font-weight: normal;
-font-size: 144px;
+font-size: 72px;
 line-height: 169px;
-margin-left: auto;
-margin-right: auto;
-color:  #FFFFFF;
+color:  #2F80ED;
 `;
+
 
 class HomeScreen extends React.Component{
     async goLogin() {
@@ -61,29 +28,37 @@ class HomeScreen extends React.Component{
 
     render(){
         return (
-                <BaseContainer>
-                    <Form>
-                        <Title>TIPTOPTournament</Title>
-                        <ButtonContainer>
-                            <ButtonWhite
-                                width="50%"
-                                onClick={() => {
-                                    this.goLogin();
-                                }}
-                            >
-                                Login
-                            </ButtonWhite>
-                        <ButtonWhite
-                            width="50%"
-                            onClick={() => {
-                                this.goRegister();
-                            }}
-                        >
-                           Registration
-                        </ButtonWhite>
-                    </ButtonContainer>
-                    </Form>
-                </BaseContainer>
+                <Container>
+                    <p className="text-center">
+                        <Row>
+                            <Col>
+                                <Title>TIPTOPTournament</Title>
+                            </Col>
+                        </Row>
+                    </p>
+                        <Row className="align-self-center">
+                             <Col xs={12} sm={12}>
+                                <Form>
+                                     <Button
+                                        type="button"
+                                        width="50%"
+                                        onClick={() => {
+                                            this.goLogin();
+                                        }}
+                                     >Login
+                                    </Button>
+                                    <Button
+                                        type="button"
+                                        width="50%"
+                                        onClick={() => {
+                                            this.goRegister();
+                                        }}
+                                    >Register
+                                    </Button>
+                                </Form>
+                             </Col>
+                        </Row>
+                </Container>
         );
     }
 }
