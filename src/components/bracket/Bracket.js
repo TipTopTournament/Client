f545c749995360f4cf4502ce4be572c9044c831e
S Anzolut <stefano.anzolut@gmail.com>
2020-04-28 18:45:05 +0200

    Bracket can now be displayed visually  FS20G01-35, FS20G01-45

--- src/components/bracket/Bracket.js
+++ src/components/bracket/Bracket.js
@@ -9,8 +9,7 @@ import Tree from "react-tree-graph";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {forEach} from "react-bootstrap/cjs/ElementChildren";
-
+import '../../views/design/custom-container.css';
 
 const TournamentBracket = styled.ul`
   margin-bottom: 50px;
@@ -25,13 +24,14 @@ const BracketContainer = styled.li`
   align-items: center;
   justify-content: center;
 `;
-const data = {name: 'Finale', children: [{name: 'Halbfinale1'}, {name: 'Halbfinale2'}]};
+
 
 class Bracket extends React.Component {
     constructor() {
         super();
         this.state = {
             games: null,
+            data: {name: 'Finale', children: [{name: 'Halbfinale1',children:[{name: "Viertelfinale"}, {name:"Viertelfinale2"}]}, {name: 'Halbfinale2'}]},
         };
     }
 
@@ -39,6 +39,73 @@ class Bracket extends React.Component {
         const {tournamentCode} = this.props.match.params;
         this.props.history.push(`/tournaments/${tournamentCode}/bracket/${game}`)
     }
+    setupBracket(Games){
+        console.log("length",Games.length);
+        switch (Games.length) {
+            case 3:
+                let setup1 = {name: Games['2']['participant1'].vorname + " " + Games['2'].score1 + " vs. " + Games['2'].score2 + " " + Games['2']['participant2'].vorname,
+                    children: [{name: Games['0']['participant1'].vorname + " " + Games['0'].score1 + " vs. " + Games['0'].score2 + " " + Games['0']['participant2'].vorname},
+                        {name: Games['1']['participant1'].vorname + " " + Games['1'].score1 + " vs. " + Games['1'].score2 + " " + Games['1']['participant2'].vorname}]};
+                this.setState({data: setup1});
+                console.log("data", this.state.data);
+                break;
+            case 7:
+                let setup2 = {name: Games['6']['participant1'].vorname + " " + Games['6'].score1 + " vs. " + Games['6'].score2 + " " + Games['6']['participant2'].vorname,
+                                    children: [{name: Games['4']['participant1'].vorname + " " + Games['4'].score1 + " vs. " + Games['0'].score2 + " " + Games['4']['participant2'].vorname,
+                                                      children: [{name: Games['0']['participant1'].vorname + " " + Games['0'].score1 + " vs. " + Games['0'].score2 + " " + Games['0']['participant2'].vorname},
+                                                                 {name: Games['1']['participant1'].vorname + " " + Games['1'].score1 + " vs. " + Games['1'].score2 + " " + Games['1']['participant2'].vorname}]},
+                                               {name: Games['5']['participant1'].vorname + " " + Games['5'].score1 + " vs. " + Games['5'].score2 + " " + Games['5']['participant2'].vorname,
+                                                      children:[{name: Games['2']['participant1'].vorname + " " + Games['2'].score1 + " vs. " + Games['2'].score2 + " " + Games['2']['participant2'].vorname},
+                                                                {name: Games['3']['participant1'].vorname + " " + Games['3'].score1 + " vs. " + Games['3'].score2 + " " + Games['3']['participant2'].vorname}]}]};
+                this.setState({data: setup2});
+                console.log("data", this.state.data);
+                break;
+            case 15:
+                let setup3 = {name: Games['14']['participant1'].vorname + " " + Games['14'].score1 + " vs. " + Games['14'].score2 + " " + Games['14']['participant2'].vorname,
+                                    children: [{name: Games['12']['participant1'].vorname + " " + Games['12'].score1 + " vs. " + Games['12'].score2 + " " + Games['12']['participant2'].vorname,
+                                                      children: [{name: Games['8']['participant1'].vorname + " " + Games['8'].score1 + " vs. " + Games['8'].score2 + " " + Games['8']['participant2'].vorname,
+                                                                        children:[{name: Games['0']['participant1'].vorname + " " + Games['0'].score1 + " vs. " + Games['0'].score2 + " " + Games['0']['participant2'].vorname},
+                                                                                  {name: Games['1']['participant1'].vorname + " " + Games['1'].score1 + " vs. " + Games['1'].score2 + " " + Games['1']['participant2'].vorname}]},
+                                                                 {name: Games['9']['participant1'].vorname + " " + Games['9'].score1 + " vs. " + Games['9'].score2 + " " + Games['9']['participant2'].vorname,
+                                                                        children:[{name: Games['2']['participant1'].vorname + " " + Games['2'].score1 + " vs. " + Games['2'].score2 + " " + Games['2']['participant2'].vorname},
+                                                                                  {name: Games['3']['participant1'].vorname + " " + Games['3'].score1 + " vs. " + Games['3'].score2 + " " + Games['3']['participant2'].vorname}]}]},
+                                               {name: Games['13']['participant1'].vorname + " " + Games['13'].score1 + " vs. " + Games['13'].score2 + " " + Games['13']['participant2'].vorname,
+                                                      children:[{name: Games['10']['participant1'].vorname + " " + Games['10'].score1 + " vs. " + Games['10'].score2 + " " + Games['10']['participant2'].vorname,
+                                                                       children:[{name: Games['4']['participant1'].vorname + " " + Games['4'].score1 + " vs. " + Games['4'].score2 + " " + Games['4']['participant2'].vorname},
+                                                                                 {name: Games['5']['participant1'].vorname + " " + Games['5'].score1 + " vs. " + Games['5'].score2 + " " + Games['5']['participant2'].vorname}]},
+                                                                {name: Games['11']['participant1'].vorname + " " + Games['11'].score1 + " vs. " + Games['11'].score2 + " " + Games['11']['participant2'].vorname,
+                                                                        children:[{name: Games['6']['participant1'].vorname + " " + Games['6'].score1 + " vs. " + Games['6'].score2 + " " + Games['6']['participant2'].vorname},
+                                                                                  {name: Games['7']['participant1'].vorname + " " + Games['7'].score1 + " vs. " + Games['7'].score2 + " " + Games['7']['participant2'].vorname}]}]}]};
+                this.setState({data: setup3});
+                console.log("data", this.state.data);
+        }
+    }
+    // setupEachGame(Games , length){
+    //     let i;
+    //     let setup;
+    //     let children;
+    //     if (length === 3) {
+    //         //children = 1;
+    //         //setup = {name: '', children: [{name: ''}, {name: ''}]}
+    //         for (i = length; i >= 0; i--) {
+    //             setup = {
+    //                 name: Games["'" + length + "'"]['participant1'].vorname + " " + Games["'" + length + "'"].score1 +
+    //                     " vs. " + Games["'" + length + "'"].score2 + " " + Games["'" + length + "'"]['participant2'].vorname
+    //             }
+    //         }
+    //     }
+    //     if (length === 7) {
+    //         //children = 3;
+    //         setup = {name: '', children: [{name: '',children: [{name: '',},{name: '',}]}, {name: '',children: [{name: '',},{name: '',}]}]}
+    //         // for (i = length; i >= 0; i--) {
+    //         //     setup = {
+    //         //         name: Games["'" + length + "'"]['participant1'].vorname + " " + Games["'" + length + "'"].score1 +
+    //         //             " vs. " + Games["'" + length + "'"].score2 + " " + Games["'" + length + "'"]['participant2'].vorname
+    //         //     }
+    //         // }
+    //     }
+    //
+    // }
     
     async componentDidMount() {
         try {
@@ -47,13 +114,16 @@ class Bracket extends React.Component {
             console.log("response", response.data);
             this.correctArray(response.data);
             this.setState({ games : response.data });
+            this.setupBracket(response.data);
+            // this.setState({data: {name: '', children: [{name: '',children: [{name: '',},{name: '',}]}, {name: '',children: [{name: '',},{name: '',}]}]}})
+
 
         } catch (error) {
             alert(`Something went wrong while fetching the users: \n${handleError(error)}`);
         }
     }
     correctArray(response){
-        const test = response.map(game => {
+        response.map(game => {
 
 
             if(!game["participant1"]){
@@ -63,8 +133,6 @@ class Bracket extends React.Component {
                 game["participant2"] = "Not yet determined!"
             }
 
-            console.log(game["participant1"]["vorname"]);
-            console.log(game["participant2"]["vorname"]);
         });
 
     }
@@ -77,10 +145,16 @@ class Bracket extends React.Component {
                 ) : (
                 <Row>
                     <Col>
-                        <Tree
-                            data={data}
-                            height={200}
-                            width={400}/>
+                        <div className = "custom-container">
+                            <Tree
+                                data={this.state.data}
+                                height={200}
+                                width={400}
+                                svgProps={{
+                                    className: 'custom',
+                                }}/>
+                                }}/>
+                        </div>
                         <TournamentBracket>
                             {this.state.games.map(game => {
                                 return (
