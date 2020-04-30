8f19183c9a4a392fa5eca7c139c6c54caae4cfe2
S Anzolut <stefano.anzolut@gmail.com>
2020-04-30 14:39:06 +0200

    Update getDomain.js

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,7 +7,7 @@ import { isProduction } from './isProduction';
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = 'https://sopra-fs20-group-01-server.herokuapp.com/';
+  const prodUrl = 'https://sopra-fs20-group-01-server.herokuapp.com';
   const devUrl = 'http://localhost:8080';
 
   return isProduction() ? prodUrl : devUrl;
