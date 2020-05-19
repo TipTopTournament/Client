e627a116b55c0cf65faddab9c9ab3e098a9a7c70
L0nyZ <tonyylyy@gmail.com>
2020-05-19 11:41:16 +0200

    FS20G01-82 added participant view function in approuter

--- src/components/shared/routers/AppRouter.js
+++ src/components/shared/routers/AppRouter.js
@@ -18,13 +18,13 @@ import { CreateTournamentGuard } from "../routeProtectors/CreateTournamentGuard"
 import PlayerList from "../../playerProfile/PlayerList";
 import ParticipantMenu from "../../participantMenu/ParticipantMenu";
 import ManagerMenu from "../../managerMenu/ManagerMenu";
-import {ManagerMenuGuard} from "../routeProtectors/ManagerMenuGuard";
-import {TournamentCodeGuard} from "../routeProtectors/TournamentCodeGuard";
-import {PlayerProfileGuard} from "../routeProtectors/PlayerProfileGuard";
-import {PlayerListGuard} from "../routeProtectors/PlayerListGuard";
-import {ParticipantMenuGuard} from "../routeProtectors/ParticipantMenuGuard";
+import { ManagerMenuGuard } from "../routeProtectors/ManagerMenuGuard";
+import { TournamentCodeGuard } from "../routeProtectors/TournamentCodeGuard";
+import { PlayerProfileGuard } from "../routeProtectors/PlayerProfileGuard";
+import { PlayerListGuard } from "../routeProtectors/PlayerListGuard";
+import { ParticipantMenuGuard } from "../routeProtectors/ParticipantMenuGuard";
 import ManagerLogin from "../../login/ManagerLogin";
-import {ManagerLoginGuard} from "../routeProtectors/ManagerLoginGuard";
+import { ManagerLoginGuard } from "../routeProtectors/ManagerLoginGuard";
 import Info from "../../homescreen/Info";
 import ManagerNavBar from "../ManagerNavBar";
 
@@ -39,137 +39,138 @@ import ManagerNavBar from "../ManagerNavBar";
  */
 
 const ManagerView = () => {
-  return(
+  return (
     <>
-    <ManagerNavBar />
-    <Switch>
-      <Route
-        path={`/manager/menu/:managerID`}
-        render={() => (
-          <ManagerMenuGuard>
-            <ManagerMenu />
-          </ManagerMenuGuard>
-        )}
-      />
-      <Route
-        path="/manager/createTournament/:managerID"
-        render={() =>(
-          <CreateTournamentGuard>
-            <CreateTournament/>
-          </CreateTournamentGuard>
-        )}
-          />
+      <ManagerNavBar />
+      <Switch>
         <Route
-              path="/manager/tournaments/:tournamentCode"
-              render={() => (
-                <TournamentGuard>
-                  <Tournament />
-                </TournamentGuard>
-              )}
-            />
-    </Switch>
+          path={`/manager/menu/:managerID`}
+          render={() => (
+            <ManagerMenuGuard>
+              <ManagerMenu />
+            </ManagerMenuGuard>
+          )}
+        />
+        <Route
+          path="/manager/createTournament/:managerID"
+          render={() => (
+            <CreateTournamentGuard>
+              <CreateTournament />
+            </CreateTournamentGuard>
+          )}
+        />
+        <Route
+          path="/manager/tournaments/:tournamentCode"
+          render={() => (
+            <TournamentGuard>
+              <Tournament />
+            </TournamentGuard>
+          )}
+        />
+      </Switch>
     </>
-  )
-}
+  );
+};
+
+const ParticipantView = () => {
+  return (
+    <>
+      <ParticipantNavBar />
+      <Switch>
+        <Route
+          path="/:tournamentCode/participantMenu"
+          render={() => (
+            <ParticipantMenuGuard>
+              <ParticipantMenu />
+            </ParticipantMenuGuard>
+          )}
+        />
+        <Route
+          path="/:tournamentCode/playerList"
+          render={() => (
+            <PlayerListGuard>
+              <PlayerList />
+            </PlayerListGuard>
+          )}
+        />
+        <Route
+          path="/:tournamentCode/participants/:participantID"
+          render={() => (
+            <PlayerProfileGuard>
+              <PlayerProfile />
+            </PlayerProfileGuard>
+          )}
+        />
+      </Switch>
+    </>
+  );
+};
 
 class AppRouter extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-            <Route path="/home" render={() => <HomeScreen />} />
-            <Route path="/info" render={() => <Info/>} />
-            <Route
-              path="/createTournament"
-              render={() => (
-                <CreateTournamentGuard>
-                  <CreateTournament />
-                </CreateTournamentGuard>
-              )}
-            />
-            <Route
-              path="/tournament"
-              render={() => (
-                <TournamentGuard>
-                  <TournamentRouter base={"/tournament"} />
-                </TournamentGuard>
-              )}
-            />
-            <Route
-              path="/register"
-              render={() => (<Registration />)}
-            />
-            <Route
-              path="/login/participant"
-              exact
-              render={() => (
-                <ParticipantLoginGuard>
-                  <ParticipantLogin />
-                </ParticipantLoginGuard>
-              )}
-            />
-              <Route
-                  path="/login/manager"
-                  exact
-                  render={() => (
-                      <ManagerLoginGuard>
-                          <ManagerLogin />
-                      </ManagerLoginGuard>
-                  )}
-              />
+          <Route path="/home" render={() => <HomeScreen />} />
+          <Route path="/info" render={() => <Info />} />
 
-            <Route path="/manager" component={ManagerView} />
+          <Route
+            path="/tournament"
+            render={() => (
+              <TournamentGuard>
+                <TournamentRouter base={"/tournament"} />
+              </TournamentGuard>
+            )}
+          />
+          <Route path="/register" render={() => <Registration />} />
+          <Route
+            path="/login/participant"
+            exact
+            render={() => (
+              <ParticipantLoginGuard>
+                <ParticipantLogin />
+              </ParticipantLoginGuard>
+            )}
+          />
+          <Route
+            path="/login/manager"
+            exact
+            render={() => (
+              <ManagerLoginGuard>
+                <ManagerLogin />
+              </ManagerLoginGuard>
+            )}
+          />
 
-            <Route path="/" exact render={() => <Redirect to={"/home"} />} />
+          <Route path="/manager" component={ManagerView} />
 
-            <Route path="/tournamentCode" render={() => (
-                <TournamentCodeGuard>
-                  <TournamentCode />
-                </TournamentCodeGuard>
-            )} />
-            
-            <Route
-              path="/:tournamentCode/leaderBoard"
-              render={() => (
-                <LeaderBoardGuard>
-                  <LeaderBoard />
-                </LeaderBoardGuard>
-              )}
-            />
-            <Route
-              path="/:tournamentCode/bracket"
-              render={() => (
-                <BracketGuard>
-                  <Bracket />
-                </BracketGuard>
-              )}
-            />
-            <Route
-              path="/:tournamentCode/participants/:participantID"
-              render={() => (
-                <PlayerProfileGuard>
-                  <PlayerProfile />
-                </PlayerProfileGuard>
-              )}
-            />
+          <Route path="/" exact render={() => <Redirect to={"/home"} />} />
 
-            <Route
-              path="/:tournamentCode/playerList"
-              render={() => (
-                <PlayerListGuard>
-                  <PlayerList />
-                </PlayerListGuard>
-              )}
-            />
+          <Route
+            path="/tournamentCode"
+            render={() => (
+              <TournamentCodeGuard>
+                <TournamentCode />
+              </TournamentCodeGuard>
+            )}
+          />
 
-            <Route
-              path="/:tournamentCode/participantMenu"
-              render={() => (
-                <ParticipantMenuGuard>
-                  <ParticipantMenu />
-                </ParticipantMenuGuard>
-              )}
-            />
+          <Route
+            path="/:tournamentCode/leaderBoard"
+            render={() => (
+              <LeaderBoardGuard>
+                <LeaderBoard />
+              </LeaderBoardGuard>
+            )}
+          />
+          <Route
+            path="/:tournamentCode/bracket"
+            render={() => (
+              <BracketGuard>
+                <Bracket />
+              </BracketGuard>
+            )}
+          />
         </Switch>
       </BrowserRouter>
     );
